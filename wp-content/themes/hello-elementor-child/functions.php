142ce323c5b5bffeb8699144961c9de831806edd
Cha-Hyperion <charlene.zybala@gmail.com>
2020-12-14 14:22:51 +0100

    css custom pour pages single

--- wp-content/themes/hello-elementor-child/functions.php
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -1,11 +1,23 @@
 <?php
 /**
-** activation theme
-**/
+ * Theme functions and definitions
+ *
+ * @package HelloElementorChild
+ */
 
-use Elementor\Core\Schemes\Color;
-
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
- wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+/**
+ * Load child theme css and optional scripts
+ *
+ * @return void
+ */
+function hello_elementor_child_enqueue_scripts() {
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		'1.0.0'
+	);
 }
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
