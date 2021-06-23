8084c4612f24e559b49b851538c85492e545d8b6
Cha-Hyperion <charlene.zybala@gmail.com>
2021-06-23 14:35:03 +0200

    retrait pixel fb

--- wp-content/themes/hello-elementor-child/inc/cookies.php
+++ wp-content/themes/hello-elementor-child/inc/cookies.php
@@ -21,21 +21,7 @@ add_action ('wp_footer', 'add_coockies_script_axeptio');
 function rgpd_cookies() { ?>
 
 	<script>
-		function launchFB(){ 
-			!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-			n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-			if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-			n.queue=[];t=b.createElement(e);t.async=!0;
-			t.src=v;s=b.getElementsByTagName(e)[0];
-			s.parentNode.insertBefore(t,s)}(window, document,'script',
-			'https://connect.facebook.net/en_US/fbevents.js');
-			fbq('init', '434866444264026');
-			fbq('set','agent','tmgoogletagmanager', '434866444264026');
-			fbq('track', 'PageView');
-			fbq('track', 'Lead');
-		}
-
-		function launchGA(){
+	function launchGA(){
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -47,9 +33,6 @@ function rgpd_cookies() { ?>
 		void 0 === window._axcb && (window._axcb = []);
 		window._axcb.push(function(axeptio) {
 			axeptio.on("cookies:complete", function(choices) {
-				if(choices.facebook_pixel) {
-					launchFB();  
-				}
 				if(choices.google_analytics) {
 					launchGA();
 				}
